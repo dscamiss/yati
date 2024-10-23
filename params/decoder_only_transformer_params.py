6ec0bdf8aa5d4d7f31b9d16859c9a7070181cca7
Scott <dscamiss@gmail.com>
2024-10-22 18:52:09 -0700

    Add missing params file

---
+++ params/decoder_only_transformer_params.py
@@ -0,0 +1,49 @@
+"""Implementation of dataclass for decoder-only transformer parameters."""
+
+from dataclasses import astuple, dataclass
+from typing import Any, Iterator
+
+from params.encoder_decoder_params import EncoderDecoderParams
+
+
+@dataclass
+class DecoderOnlyTransformerParams:
+    """Dataclass for decoder-only transformer parameters.
+
+    Args:
+        d_model (int): Input/output embedding dimension.
+        max_seq_len (int): Maximum input sequence length.
+        input_num_embeddings (int): Input vocabulary size.
+        output_num_embeddings (int): Output vocabulary size.
+        decoder_stack_num_layers (int): Number of decoder layers in decoder stack.
+        decoder_params (EncoderDecoderParams): Decoder layer parameters.
+        p_dropout (float): Dropout probability; used in positional encoding layer.
+        tie_weight_matrices (bool): Tie weight matrices in input embedding and pre-softmax layers.
+
+    Raises:
+        ValueError: If tie_weight_matrices is True and there is a mismatch between the input and
+            output vocabulary sizes.
+
+    Note:
+        To tie weight matrices, we need the input vocabulary size and the output vocabulary size to
+        be equal.
+    """
+
+    d_model: int
+    max_seq_len: int
+    input_num_embeddings: int
+    output_num_embeddings: int
+    decoder_stack_num_layers: int
+    decoder_params: EncoderDecoderParams
+    p_dropout: float
+    tie_weight_matrices: bool
+
+    def __iter__(self) -> Iterator[Any]:
+        return iter(astuple(self))
+
+    def __post_init__(self) -> None:
+        num_embeddings_mismatch = self.input_num_embeddings != self.output_num_embeddings
+        if self.tie_weight_matrices and num_embeddings_mismatch:
+            raise ValueError(
+                "Need input_num_embeddings == output_num_embeddings to tie weight matrices"
+            )
